07848f3b474862ecd37e9a5f67dc2f11ecaab66d
MascarBone <simone.bonelli@gmail.com>
2021-09-29 16:19:36 +0200

    Created vue-dischi, components setting

---
+++ src/components/Collection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-2" v-for="(element,index) in listAlbum" :key="index">
+        <Disc :discItem="element"/>
+      </div>
+    </div>  
+  </div>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+import Disc from './Disc.vue';
+
+export default {
+  name: 'Collection',
+
+  data: function () {
+    return {
+      listAlbum: [],
+    }
+  },
+
+  components: {
+    Disc
+  },
+
+  created: function() {
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((reply) => {
+      console.log(reply.data.response);
+      this.listAlbum = [...reply.data.response];
+      console.log(this.listAlbum);
+    })
+  }
+}
+</script>
+
+<style>
+/* @import "~bootstrap/scss/bootstrap-grid"; */
+
+</style>
---
+++ src/components/Disc.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="disc">
+      <div class="disc_img">
+          <img :src="discItem.poster" :alt="discItem.title + ' - Album image'">
+      </div>
+      <div class="disc_text">
+          <h2>{{discItem.title}}</h2>
+          <p>{{discItem.author}}</p>
+          <p>{{discItem.year}}</p>
+          <p>{{discItem.genre}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Disc',
+    
+    // props: {
+    //     poster: String,
+    //     title: String,
+    //     author: String,
+    //     genre: String,
+    //     year: Number,
+    // },
+
+    props: {
+        discItem: Object,
+    },
+
+    data: function() {
+        return {
+            vuoto: '',
+        }
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+.disc_img{
+    // height: 250px;
+    // display: flex;
+    // align-items: center;
+
+
+    img {
+        width: 100%;
+    }
+}
+
+</style>
